0efb61279fd41824db9165f1b29736339d141afc
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2020-03-03 20:27:45 +0300

    add password hash

--- app/crud.py
+++ app/crud.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 
-from . import models, schemas
+from . import models, schemas, security
 
 
 def get_user(db: Session, user_id: int):
@@ -16,12 +16,21 @@ def get_users(db: Session, skip: int = 0, limit: int = 100):
 
 
 def create_user(db: Session, user: schemas.UserCreate):
-    hashed_password = f"kek {user.password}"
+    hashed_password = security.get_password_hash(user.password)
     new_user = models.User(email=user.email, hashed_password=hashed_password)
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
     return new_user
+
+
+def authenticate_user(db: Session, email: str, password: str):
+    user = get_user_by_email(db, email)
+    if not user:
+        return None
+    if not security.verify_password(password, user.hashed_password):
+        return None
+    return user
 
 
 def get_items(db: Session, skip: int = 0, limit: int = 100):
